94ff4afde06cc746d7d137b8925096a89a2c63bb
mrendy58 <derendyyy58@gmail.com>
2024-12-14 17:46:11 +0700

    membuat metode untuk memanggil fungsi getMahasiswa dari MahasiswaDao

--- app/src/main/java/com/example/p7meeting9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/p7meeting9/repository/LocalRepositoryMhs.kt
@@ -15,5 +15,9 @@ class LocalRepositoryMhs(
         return mahasiswaDao.getALLMahasiswa()
     }
 
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
 
 }
