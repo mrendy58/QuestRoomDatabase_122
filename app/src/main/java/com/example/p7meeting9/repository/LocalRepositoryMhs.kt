62e234388a53426b960e84d52b34819678d3dd68
mrendy58 <derendyyy58@gmail.com>
2024-12-14 17:47:32 +0700

    membuat metode untuk memanggil fungsi updateMahasiswa dari MahasiswaDao

--- app/src/main/java/com/example/p7meeting9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/p7meeting9/repository/LocalRepositoryMhs.kt
@@ -23,5 +23,7 @@ class LocalRepositoryMhs(
         mahasiswaDao.deleteMahasiswa(mahasiswa)
     }
 
-
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
