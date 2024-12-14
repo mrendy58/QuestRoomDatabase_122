08e81b6aa5190c4c6973ef422595f12c9ee3cdac
mrendy58 <derendyyy58@gmail.com>
2024-12-14 17:22:16 +0700

    membuat metode untuk memanggil fungsi getALLMahasiswa dari MahasiswaDao

---
+++ app/src/main/java/com/example/p7meeting9/repository/RepositoryMhs.kt
@@ -0,0 +1,13 @@
+package com.example.p7meeting9.repository
+
+import com.example.p7meeting9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryMhs {
+    suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+
+}
