537c54fd4bd9d3dc312a961fa30e8bcbfbcada55
mrendy58 <derendyyy58@gmail.com>
2024-12-14 17:59:05 +0700

    membuat object route untuk halaman update

--- app/src/main/java/com/example/p7meeting9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/p7meeting9/ui/navigation/AlamatNavigasi.kt
@@ -13,4 +13,10 @@ interface AlamatNavigasi {
         val routesWithArg = "$route/{$NIM}"
     }
 
+    object DestinasiUpdate : AlamatNavigasi{
+        override val route = "update"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
+
 }
