354eb658aa4f27a214476ae7e29ac25f1810e0ed
mrendy58 <derendyyy58@gmail.com>
2024-12-16 09:21:51 +0700

    revisiDestinasi Halaman

--- app/src/main/java/com/example/p7meeting9/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/p7meeting9/ui/viewmodel/UpdateMhsViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.p7meeting9.data.entity.Mahasiswa
 import com.example.p7meeting9.repository.RepositoryMhs
+import com.example.p7meeting9.ui.navigation.AlamatNavigasi
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -20,7 +21,7 @@ class UpdateMhsViewModel(
     var updateUIState by mutableStateOf(MhsUIState())
         private set
 
-    private val _nim: String = checkNotNull(savedStateHandle[DestinasiEdit.NIM])
+    private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiUpdate.NIM])
 
     init {
         viewModelScope.launch {
